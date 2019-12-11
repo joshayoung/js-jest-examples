0c68a499f2d6b93fb2d7c7c7651efb2a411e0fe2
Josh Young <j22cal@gmail.com>
2019-12-11 12:07:02 -0600

    Add some basic file structure for viewing in the browser

--- scripts/example.js
+++ scripts/example.js
@@ -1,7 +1,22 @@
 export default class Example {
-    constructor() { }
-  
-    test() {
-      return "test";
+  constructor() { }
+
+  test() {
+    return "test";
+  }
+
+  getData(url) {
+    let token = window.localStorage.getItem('token');
+    return fetch(url
+    , {headers: {'Authorization': 'token ' + token},}
+    ).then((response) => {
+      return response.json();
     }
+    );
   };
+
+  load() {
+    let data = this.getData('https://api.github.com/users/joshayoung/repos')
+    document.getElementById('data').innerHTML = data;
+  }
+};
---
+++ scripts/load.js
@@ -0,0 +1,13 @@
+import Api from "./api.js";
+
+let api = new Api();
+
+// Get github repo list
+api.repoActivity();
+
+
+import Example from "./example.js"
+
+let example = new Example();
+
+example.load();
