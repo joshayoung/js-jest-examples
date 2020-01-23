f810b4d5722ebb4e8ce1ee230e98ab51cd52fa7b
Josh Young <j22cal@gmail.com>
2020-01-23 11:48:19 -0600

    Tests with dependency injection

--- video_examples/sandbox.js
+++ video_examples/sandbox.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const fetch = require('node-fetch')
 
-function getData(fetch) {
-    fetch('https://api.github.com/users/joshayoung/repos',
-    { headers: { Authorization: "token " }} 
-    )
-    return Promise.resolve("test")
+function getData(fetch, process) {
+    return fetch('https://api.github.com/users/joshayoung/repos',
+    { headers: { Authorization: "token " + process.env.TOKEN }} 
+    ).then(response => response.json())
+    .then(data => data[0].id)
+    // .then(dat => console.log(dat))
+    // return Promise.resolve("test")
 }
+
+const result = getData(fetch, process);
+
+console.log(result)
 
 module.exports = getData;
--- video_examples/sandbox.test.js
+++ video_examples/sandbox.test.js
@@ -2,11 +2,27 @@ const getData = require('./sandbox')
 
 it('calls github.com correctly', () => {
   let isMockedFetchCalled = false
-  const mockedFetch = (url) => {
-    expect(url).toBe('https://api.github.com/users/joshayoung/repos')
-    isMockedFetchCalled = true
+  const fakeProcess = {
+    env: {
+      TOKEN: 123
+    }
   }
-  getData(mockedFetch).then(result => {
+  const mockedFetch = (url, options) => {
+    expect(url).toBe('https://api.github.com/users/joshayoung/repos')
+    expect(options.headers.Authorization).toBe('token ' + fakeProcess.env.TOKEN)
+    isMockedFetchCalled = true
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        rates: {
+          standard: {
+            value: 19
+          }
+        }
+
+      })
+    })
+  }
+  getData(mockedFetch, fakeProcess).then(result => {
     expect(isMockedFetchCalled).toBe(true)
   })
 })
